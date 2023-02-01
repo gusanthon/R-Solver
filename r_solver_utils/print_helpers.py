631842de03ccb7321f0fdd7abf72ded2e363637f
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-02-01 15:23:26 -0500

    Generate S matrix for pywdf circuit

--- r_solver_utils/print_helpers.py
+++ r_solver_utils/print_helpers.py
@@ -1,9 +1,10 @@
-import subprocess, sys, re
+import subprocess, sys, re, os
 
 def print_matrix(M, out_file, num_ports, adapt_expr, args, custom_args):
     '''Prints a Sage matrix as a C-style 2D array'''
-    
-    comments = '// This scattering matrix was derived using the R-Solver python script (https://github.com/jatinchowdhury18/R-Solver),\n'
+
+    comment_symbol = '# ' if args.pywdf else '// '
+    comments = comment_symbol + 'This scattering matrix was derived using the R-Solver python script (https://github.com/jatinchowdhury18/R-Solver),\n'
 
     if custom_args:
         args_str = 'r_solver.py '
@@ -12,16 +13,24 @@ def print_matrix(M, out_file, num_ports, adapt_expr, args, custom_args):
         args_str += '--verbose ' + str(args.verbose) + ' '
         args_str += '--out ' + args.out_file.name + ' '
         args_str += args.netlist.name
-        comments += '// invoked with command: ' + args_str + '\n'
+        args_str += str(args.pywdf) + ' '
+        comments += comment_symbol + 'invoked with command: ' + args_str + '\n'
     else:
-        args = subprocess.list2cmdline(sys.argv[:])
-        comments += '// invoked with command: ' + args + '\n'
-    prefix = 'const auto S_matrix[' + str(num_ports) + '][' + str(num_ports) + '] = {'
-    empty_prefix = ' ' * len(prefix)
+        args_str = subprocess.list2cmdline(sys.argv[:])
+        comments += comment_symbol + 'invoked with command: ' + args_str + '\n'
     
     M_strs = M.str(rep_mapping=lambda a: str(a) + ',')
-    M_strs = M_strs.replace('[', empty_prefix + '{').replace(',]', '},')
-    M_strs = comments + prefix + M_strs[len(prefix):-1] + '};'
+
+    if args.pywdf:
+        M_strs = M_strs.replace('\n',',\n\t')
+        M_strs = comments + 'R.set_S_matrix([' + M_strs + ']\n)'
+        
+    else:
+        prefix = 'const auto S_matrix[' + str(num_ports) + '][' + str(num_ports) + '] = {'
+        empty_prefix = ' ' * len(prefix)
+        M_strs = M_strs.replace('[', empty_prefix + '{').replace(',]', '},')
+        M_strs = comments + prefix + M_strs[len(prefix):-1] + '};'
+    
 
     # C syntax can't do exponents using the '^' operator.
     # This is a stupid hack that assumes all resistors have
@@ -35,12 +44,23 @@ def print_matrix(M, out_file, num_ports, adapt_expr, args, custom_args):
 
     if out_file is not None:
         if adapt_expr is not None:
-            M_strs += '\n\n' + str(adapt_expr)
+            adapt_expr = str(adapt_expr).replace('==', '=')
+            M_strs += '\n\n' + adapt_expr
+            adapt_var = adapt_expr[:2] 
+            M_strs += '\nreturn ' + adapt_var
+            if not args.pywdf:
+                M_strs += ';'
+            
         out_file.write(M_strs)
+        cmd = 'open ' + args.out_file.name
+        os.system(cmd)
+
     else:
         print('')
         print('Scattering matrix:')
         print(M_strs)
+    
+    return M_strs
 
 def print_shape(M):
     '''Prints the shape of a Sage matrix'''
